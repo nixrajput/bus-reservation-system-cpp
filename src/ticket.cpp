f4843f2e126f052d171e1abfcafeed04b773b225
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2021-12-04 23:02:15 +0530

    update changes and bug fixes

--- src/ticket.cpp
+++ src/ticket.cpp
@@ -1,19 +1,18 @@
-#include<iostream>
+#include <iostream>
 
 #include "ticket.h"
 
 using namespace std;
 
 void Ticket::generateTicket(char cname[], Admin ab)
-    {
-        strcpy(name,cname);
-        a = ab;
-    }
+{
+    strcpy(name, cname);
+    a = ab;
+}
 
 void Ticket::displayTicket()
-    {
-        cout<<"\t\t\t\t\t\t\t\t\t\t-------------------------------------------------\n";
-        cout <<"\t\t\t\t\t\t\t\t\t\tName: " << name;
-        a.showTicket();
-
-    }
+{
+    cout << "\t\t\t\t\t\t\t\t\t\t-------------------------------------------------\n";
+    cout << "\t\t\t\t\t\t\t\t\t\tName: " << name;
+    a.showTicket();
+}
